4bb812a3ccfb7b9176dc39e27ef453aaa89a65d4
Paul Value <paulvalue@gmail.com>
2025-05-14 22:36:44 +0300

    Initial commit

---
+++ src/main/java/com/example/testtops/TesttopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesttopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testtops/TesttopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesttopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
